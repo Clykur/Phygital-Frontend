9d051c28d6c32dc56194154d69b20050e6e98480
chinnuk0521 <chinnuk0521@gmail.com>
2026-05-04 18:25:05 +0530

    APi fix

--- artifacts/api-server/api.js
+++ artifacts/api-server/api.js
@@ -1,4 +1,9 @@
 import app from './dist/index.mjs';
 
+// Add a temporary test route to confirm the app is running
+app.get('/test-route', (req, res) => {
+  res.send('Hello from Vercel API Test Route!');
+});
+
 export default app;
 export const handler = app;
