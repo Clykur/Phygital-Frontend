433cb85bd7292d77bd200971bfba1cb2f65c6503
chinnuk0521 <chinnuk0521@gmail.com>
2026-05-04 19:23:22 +0530

    Fix: api.js to correctly export handler from index.mjs default export

--- artifacts/api-server/api.js
+++ artifacts/api-server/api.js
@@ -1,7 +1,7 @@
-import { handler } from './index.mjs';
+import app from './index.mjs';
 
 // The console.log was for debugging deployment issues, can be removed now.
 // console.log('Vercel API.js: Attempting to load app from index.mjs');
 
-export default handler;
-export { handler };
+export default app;
+export const handler = app;
